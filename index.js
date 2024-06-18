19de03efea20e609da0651b209dabca0b491d9ac
Manish Jangid - EP Soft <manish_jangid@eplanetsoft.com>
2024-06-18 15:17:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 var http = require('http');
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/mydatabase';
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true, // Optional, adds support for unique indexes
+});
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 //create a server object:
 http.createServer(function (req, res) {
